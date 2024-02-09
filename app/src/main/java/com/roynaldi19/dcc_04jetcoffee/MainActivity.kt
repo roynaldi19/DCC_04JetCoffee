c7b84fef45283b421ac71d9b60e442320fd306e9
roynaldi19 <roynaldi19@gmail.com>
2024-02-09 18:14:34 +0700

    add Banner, Caterory, Menu Favorit, Menu Terlaris.

--- app/src/main/java/com/roynaldi19/dcc_04jetcoffee/MainActivity.kt
+++ app/src/main/java/com/roynaldi19/dcc_04jetcoffee/MainActivity.kt
@@ -3,13 +3,30 @@ package com.roynaldi19.dcc_04jetcoffee
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.roynaldi19.dcc_04jetcoffee.model.Menu
+import com.roynaldi19.dcc_04jetcoffee.model.dummyBestSellerMenu
+import com.roynaldi19.dcc_04jetcoffee.model.dummyCategory
+import com.roynaldi19.dcc_04jetcoffee.model.dummyMenu
+import com.roynaldi19.dcc_04jetcoffee.ui.component.CategoryItem
+import com.roynaldi19.dcc_04jetcoffee.ui.component.MenuItem
+import com.roynaldi19.dcc_04jetcoffee.ui.component.Search
+import com.roynaldi19.dcc_04jetcoffee.ui.component.SectionText
 import com.roynaldi19.dcc_04jetcoffee.ui.theme.DCC_04JetCoffeeTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +43,63 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun JetCoffeeApp() {
+    Column {
+        Banner()
+        SectionText(stringResource(R.string.section_category))
+        CategoryRow()
+        SectionText(stringResource(R.string.section_favorite_menu))
+        MenuRow(dummyMenu)
+        SectionText(stringResource(R.string.section_best_seller_menu))
+        MenuRow(dummyBestSellerMenu)
+    }
+}
 
+@Composable
+fun Banner(
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier = modifier) {
+        Image(
+            painter = painterResource(R.drawable.banner),
+            contentDescription = "Banner Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.height(160.dp)
+        )
+        Search()
+    }
+}
+
+@Composable
+fun CategoryRow(
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+
+    )
+    {
+        items(dummyCategory, key = { it.textCategory }) { category ->
+            CategoryItem(category)
+        }
+
+    }
+}
+
+@Composable
+fun MenuRow(
+    listMenu: List<Menu>,
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ) {
+        items(listMenu, key = {it.title}) { menu ->
+            MenuItem(menu)
+        }
+    }
 }
 
 @Preview(showBackground = true)
